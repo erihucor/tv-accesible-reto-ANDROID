c33e0643d9e9e9b6f0c8168d2ccecc3eb78607fa
erihucor <ericksitocg@hotmail.com>
2026-08-04 18:05:23 -0500

    ECG: fix enable buttons to channels

--- app/src/main/java/com/example/tvaccesibleandroid/MainActivity.kt
+++ app/src/main/java/com/example/tvaccesibleandroid/MainActivity.kt
@@ -44,10 +44,15 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Row
 import androidx.compose.ui.Alignment
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import android.view.KeyEvent
 
 class MainActivity : ComponentActivity() {
 
     private var wakeLock: PowerManager.WakeLock? = null
+
+    val channels =  ChannelsProvider.channels
+
+    var currentIndex by mutableStateOf(0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
@@ -71,13 +76,7 @@ class MainActivity : ComponentActivity() {
         wakeLock?.acquire(10 * 60 * 1000L)
 
         setContent {
-            val channels = remember {
-                ChannelsProvider.channels
-            }
 
-            var currentIndex by remember {
-                mutableStateOf(0)
-            }
             val currentChannel = channels[currentIndex]
 
             // Oculta status bar y navegación en Compose
@@ -105,6 +104,31 @@ class MainActivity : ComponentActivity() {
             if (it.isHeld) it.release()
         }
         super.onDestroy()
+    }
+
+    //Handle control remote buttons
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+
+        Toast.makeText(
+            this,
+            "${KeyEvent.keyCodeToString(keyCode)} ($keyCode)",
+            Toast.LENGTH_SHORT
+        ).show()
+
+        when (keyCode) {
+
+            KeyEvent.KEYCODE_DPAD_UP -> {
+                currentIndex = (currentIndex - 1 + channels.size) % channels.size
+                return true
+            }
+
+            KeyEvent.KEYCODE_DPAD_DOWN -> {
+                currentIndex = (currentIndex + 1) % channels.size
+                return true
+            }
+        }
+
+        return super.onKeyDown(keyCode, event)
     }
 }
 
